6da6d3b6030ec82845fd623fd745dc6063d191f3
Serhii Havrylenko <prostolms@outlook.com>
2025-05-20 22:27:35 -1000

    Prepared to push

--- PralnaDemo/Models/Entities.Designer.cs
+++ PralnaDemo/Models/Entities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\inetpub\wwwroot\Pralna\Source\PralnaDemo\Models\Entities.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Doc\Projects\GitHubTest_CentralnaPralna\Centralna-Pralna\PralnaDemo\Models\Entities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
